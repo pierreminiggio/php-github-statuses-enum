1f26a833b305cbd3423175dc72adca1429ad0d42
Pierre Miniggio <pierre.miniggio@gmail.com>
2021-04-28 19:16:21 +0200

    first commit

---
+++ src/ConclusionsEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PierreMiniggio\GithubStatusesEnum;
+
+class ConclusionsEnum
+{
+    public const ACTION_REQUIRED = 'action_required';
+    public const CANCELLED = 'cancelled';
+    public const FAILURE = 'failure';
+    public const NEUTRAL = 'neutral';
+    public const SUCCESS = 'success';
+    public const SKIPPED = 'skipped';
+    public const STALE = 'stale';
+    public const TIMED_OUT = 'timed_out';
+}
---
+++ src/GithubStatusesEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PierreMiniggio\GithubStatusesEnum;
+
+class GithubStatusesEnum
+{
+    public const QUEUED = 'queued';
+    public const IN_PROGRESS = 'in_progress';
+    public const COMPLETED = 'completed';
+}
